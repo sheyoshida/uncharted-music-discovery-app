dc27ebf4d31814a9db51be773173bba21bdf1ab5
shenayoshida <sheyoshida@gmail.com>
2015-11-08 16:11:20 -0500

    added empty xcode project

---
+++ unit-3-final-project/unit-3-final-project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  unit-3-final-project
+//
+//  Created by Shena Yoshida on 11/8/15.
+//  Copyright © 2015 Shena Yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ unit-3-final-project/unit-3-final-project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  unit-3-final-project
+//
+//  Created by Shena Yoshida on 11/8/15.
+//  Copyright © 2015 Shena Yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
