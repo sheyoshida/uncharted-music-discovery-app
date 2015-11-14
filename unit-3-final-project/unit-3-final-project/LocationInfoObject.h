8a3bdef64574bbf45b94e67baeb7e2cc09bdafd2
Henna <henna.ahmed92@gmail.com>
2015-11-14 14:58:43 -0500

    looping through locations

---
+++ unit-3-final-project/unit-3-final-project/LocationInfoObject.h
@@ -0,0 +1,15 @@
+//
+//  LocationInfoObject.h
+//  unit-3-final-project
+//
+//  Created by Henna Ahmed on 11/14/15.
+//  Copyright © 2015 Shena Yoshida. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationInfoObject : NSObject
+    @property (nonatomic) NSString * SubAdministrativeArea ;
+    @property (nonatomic) NSString * State;
+    @property (nonatomic) NSString * Sublocality;
+@end
---
+++ unit-3-final-project/unit-3-final-project/LocationInfoObject.m
@@ -0,0 +1,14 @@
+//
+//  LocationInfoObject.m
+//  unit-3-final-project
+//
+//  Created by Henna Ahmed on 11/14/15.
+//  Copyright © 2015 Shena Yoshida. All rights reserved.
+//
+
+#import "LocationInfoObject.h"
+
+@implementation LocationInfoObject
+
+
+@end
